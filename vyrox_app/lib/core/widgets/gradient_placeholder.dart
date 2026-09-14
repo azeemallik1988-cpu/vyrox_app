aca0c3ae167410d1e073c10572fb98c0ca32f0e7
azeemallik1988-cpu <azeemallik1988@gmail.com>
2026-09-14 10:25:15 +0530

    Create GradientPlaceholder widget
    Added GradientPlaceholder widget for customizable gradient backgrounds.

--- vyrox_app/lib/core/widgets/gradient_placeholder.dart
+++ vyrox_app/lib/core/widgets/gradient_placeholder.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
 
+class GradientPlaceholder extends StatelessWidget {
+  const GradientPlaceholder({
+    super.key,
+    required this.seed,
+    this.borderRadius = 16,
+  });
+
+  final int seed;
+  final double borderRadius;
+
+  Color _color(int salt) {
+    final hue = ((seed + salt) % 360).toDouble();
+    return HSVColor.fromAHSV(1, hue, 0.55, 0.38).toColor();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(borderRadius),
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [_color(0), _color(80), _color(160)],
+        ),
+      ),
+    );
+  }
+}
