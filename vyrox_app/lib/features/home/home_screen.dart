9b67af2b5039e1c3079d775d091f7fc9e5971838
azeemallik1988-cpu <azeemallik1988@gmail.com>
2026-09-16 21:29:27 +0530

    Increase height of SizedBox from 40 to 50

--- vyrox_app/lib/features/home/home_screen.dart
+++ vyrox_app/lib/features/home/home_screen.dart
@@ -112,7 +112,7 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
 
-              const SizedBox(height: 40),
+              const SizedBox(height: 50),
             ],
           ),
         ),
