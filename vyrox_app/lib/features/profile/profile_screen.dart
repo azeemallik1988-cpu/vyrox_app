43f710a1a6178c15e872e0ba533dd31c3e88d260
azeemallik1988-cpu <azeemallik1988@gmail.com>
2026-09-17 01:08:02 +0530

    Fix typo in CreateToolScreen argument

--- vyrox_app/lib/features/profile/profile_screen.dart
+++ vyrox_app/lib/features/profile/profile_screen.dart
@@ -110,7 +110,7 @@ class ProfileScreen extends StatelessWidget {
         currentIndex: 4,
         onTap: (i) {
           if (i == 0) Navigator.push(context, MaterialPageRoute(builder: (_) => const HomeScreen()));
-          else if (i == 1) Navigator.push(context, MaterialPageRoute(builder: (_) => const CreateToolScreen(tool: CreationType.image)));
+          else if (i == 1) Navigator.push(context, MaterialPageRoute(builder: (_) => const CreateToolScreen(tool: CreationType 'Image' )));
           else if (i == 2) Navigator.pushNamed(context, '/explore');
           else if (i == 3) Navigator.push(context, MaterialPageRoute(builder: (_) => const CreationsScreen()));
         },
